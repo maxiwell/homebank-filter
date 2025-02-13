25de000488185cabb497a379bc8d79a7ea9b61c2
Maxiwell S. Garcia <maxiwell@gmail.com>
2025-02-13 17:56:09 -0300

    jql: add date support

--- filter.py
+++ filter.py
@@ -3,6 +3,47 @@ import numpy as np
 import pyparsing as pp
 import sys
 
+from datetime import datetime, timedelta
+
+# Converte um timestamp baseado em dias desde 0000-01-01 para uma data no formato YYYY-MM-DD.
+def ts_to_date(timestamp: int, formato) -> str:
+
+    # O Python começa em 0001-01-01
+    base_date = datetime(1, 1, 1)
+
+    # -1 para compesar a diferenca entre o padrao do MATLAB e do Python
+    converted_date = base_date + timedelta(days=timestamp - 1)
+    return converted_date.strftime(formato)
+
+
+def date_to_ts(date) -> int:
+    # O Python começa em 0001-01-01
+    base_date = datetime(1, 1, 1)
+
+    formatos = ["%d/%m/%Y", "%m/%Y", "%Y"]
+    for formato in formatos:
+        try:
+            ts = datetime.strptime(date, formato)
+            # -1 para compesar a diferenca entre o padrao do MATLAB e do Python
+            converted_date = (ts - base_date).days - 1
+            return converted_date
+        except ValueError:
+            continue
+    raise ValueError(f"Formato de data inválido: {date}")
+
+
+def comparar_datas(data1: str, data2: str) -> str:
+    formato = "%d/%m/%Y"
+    dt1 = datetime.strptime(data1, formato)
+    dt2 = datetime.strptime(data2, formato)
+
+    """
+    data2 > data1 = positivo
+    data1 > data2 = negativo
+    data1 = data2 = igual
+    """
+    return (dt2 - dt1).days
+
 def carregar_dados(arquivo):
     try:
         tree = ET.parse(arquivo)
@@ -13,14 +54,18 @@ def carregar_dados(arquivo):
         return None
 
 def criar_parser():
-    campo = pp.oneOf("descricao valor categoria")
+    campo = pp.oneOf("descricao valor categoria data")
+
     operador = pp.oneOf("= >= <= < > == != <> ~")
     not_op = pp.oneOf("NOT not !")
     and_op = pp.oneOf("AND and")
     or_op = pp.oneOf("OR or")
+
     valor_str = pp.QuotedString("'")
     valor_num = pp.Word(pp.nums + ".").setParseAction(lambda t: float(t[0]))
+
     valor = valor_str | valor_num
+
     expressao = pp.Group(campo + operador + valor)
 
     condicao = pp.infixNotation(expressao, [
@@ -37,12 +82,16 @@ def get_contexto(transacao, root):
     descricao_trans = transacao.get("wording", "").lower()
     valor = transacao.get("amount", "")
     categoria = transacao.get("category", "")
+    date = transacao.get("date", "")
+
+    date = ts_to_date(int(date), "%d/%m/%Y")
 
     cat = root.find(f".//cat[@key='{categoria}']")
     if cat is not None:
         categoria = cat.get("name")
 
-    return {"categoria": categoria,
+    return {"data": date,
+            "categoria": categoria,
             "descricao": descricao_trans,
             "valor": valor}
 
@@ -65,15 +114,21 @@ def avaliar_expressao(transacao, root, parsed_query):
             operador, op_dir = parsed
 
         # special operators
-        if operador.upper == "NOT" or operador == "!":
+        if operador.upper() == "NOT" or operador == "!":
             return not avaliar(op_dir)
         elif operador.upper() == "AND":
             return avaliar(op_esq) and avaliar(op_dir)
         elif operador.upper() == "OR":
             return avaliar(op_esq) or avaliar(op_dir)
 
+        # campo_valor has the right side value from homebank file
         campo_valor = contexto.get(op_esq, "")
-        if (isinstance(op_dir, float)):
+
+        # semantic exceptions
+        if (op_esq == "data"):
+            campo_valor = date_to_ts(campo_valor)
+            op_dir      = date_to_ts(op_dir)
+        elif (isinstance(op_dir, float)):
             campo_valor = float(campo_valor)
 
         if operador == "~":
@@ -111,14 +166,15 @@ def buscar_transacoes(root, query):
     return transacoes
 
 if __name__ == "__main__":
-    query = sys.argv[1]
     arquivo = "Gastos.xhb"  # Substitua pelo caminho correto do arquivo
     root = carregar_dados(arquivo)
 
     if root is not None:
         # Exemplo de consulta JQL-like
+        #query = sys.argv[1]
         #query = "((descricao ~ 'pix' AND valor >= 150) AND valor <= 200)"
-        #query = "(descricao ~ 'pix' AND (valor == 15 OR valor == 200)) AND NOT descricao ~ 'guilher'"
+        query = "(descricao ~ 'pix' AND (valor == 15 OR valor == 200)) AND ! descricao ~ 'guilher'"
+        #query = "descricao ~ 'MOBILE PAG TIT BANCO 481' AND data < '01/2025'"
         resultado = buscar_transacoes(root, query)
         for r in resultado:
             print(r)
